aafa210fba93a5119765df4b755c1d8d05b74ae5
Simon20060610 <wqxxchenziyi@163.com>
2026-07-21 21:06:13 +0800

    修改了自我介绍

--- astro-blog/src/data/site.ts
+++ astro-blog/src/data/site.ts
@@ -1,5 +1,5 @@
 export const site = {
-  title: 'SimonChen 的技术小站',
+  title: '观测记录',
   description: '记录嵌入式、RT-Thread、ROS2 与开发实践。',
   author: 'SimonChen',
   github: 'https://github.com/Simon20060610',
